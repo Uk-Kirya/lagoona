fd379c817b2b3002653044a8347cfb7a1efaafdd
Kirill Losev <udarnik.kirill@gmail.com>
2026-05-25 19:30:44 +0300

    Change docker-compose.yml file

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,4 +12,4 @@ echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
 echo "Starting Gunicorn with $WORKERS workers..."
-exec gunicorn kd.wsgi:application --bind 0.0.0.0:8000 --workers $WORKERS
+exec gunicorn logoona.wsgi:application --bind 0.0.0.0:8000 --workers $WORKERS
