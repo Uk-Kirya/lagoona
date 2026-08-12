bd2f6e2c5e662c6621e874f69c298f677be298b9
Kirill Losev <udarnik.kirill@gmail.com>
2026-08-12 23:49:18 +0300

    123

--- logoona/settings.py
+++ logoona/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x48!*$$l(#*uahm-r&1ofp-+a0!7z-^(z6z$(l1^do*&&pkao@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'lagoona.lc',
